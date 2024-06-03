c16e013f2d25237dd976e366a4c1aabafe24f8c3
Davi Miranda <davimiranda210@gmail.com>
2024-06-03 12:39:22 -0300

    Update Integração numerica.py

--- Integração numerica.py
+++ Integração numerica.py
@@ -1,39 +1,46 @@
 # Definição da função
 def f(x):
-    return 0.02*x**6 - 0.36*x**5 + 3.93*x**4 - 25.29*x**3 + 87.78*x**2 - 124.23*x
-
+    return x*x
 # Dados de entrada
 a = 1
-b = 38
-n = 100  # Definindo um número inteiro de subintervalos
+b = 3
+n = 4 # Definindo um número inteiro de subintervalos
 h = (b - a) / n
 
+xm = a+h/2
 # Inicializando as somas
 soma_inferior = 0
 soma_superior = 0
 soma_media = 0
 
 # Aproximação Inferior
-for i in range(n):
-    xi = a + i * h
-    soma_inferior += f(xi)
+for i in range(n-1):
+    xi = f(a) + h
+    soma_inferior += xi
+    
 
 # Aproximação Superior
-for i in range(1, n + 1):
-    xs = a + i * h
-    soma_superior += f(xs)
+for i in range(n-1):
+    xs = f(a + h) 
+    soma_superior += xs
+    
 
 # Aproximação dos Pontos Médios
-for i in range(n):
-    xm = a + (i + 0.5) * h
-    soma_media += f(xm)
+for i in range(n-1):
+    xm = f(a+h/2) + h
+    soma_media += xm
+    print(soma_media)
+ 
 
 # Multiplicando pela largura dos subintervalos
-aprox_inferior = h * soma_inferior
-aprox_superior = h * soma_superior
-aprox_media = h * soma_media
+aprox_inferior = h * soma_inferior + f(a)
+aprox_superior = h * soma_superior + f(b)
+aprox_media = h * soma_media + f(a+h/2)
 
 # Imprimindo os resultados
 print(f'Aproximação Inferior: {aprox_inferior:.4f}')
 print(f'Aproximação Superior: {aprox_superior:.4f}')
 print(f'Aproximação dos Pontos Médios: {aprox_media:.4f}')
+print({soma_media})
+print(h)
+print(a)
